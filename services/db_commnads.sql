5d00ad99a41ff9bdeac00e1e1d93dbce69146d89
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2023-07-31 20:02:49 +0530

    Update and rename db_commnads.md to db_commnads.sql

--- services/db_commnads.sql
+++ services/db_commnads.sql
@@ -1,3 +1,14 @@
+postgres=# CREATE TABLE student (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INTEGER,
+    marks FLOAT,
+    grade CHAR(1)
+);
+
+
+
+
 postgres=# INSERT INTO student (name, age, marks, grade) VALUES
     ('John Doe', 20, 85, 'B'),
     ('Jane Smith', 22, 92, 'A'),
