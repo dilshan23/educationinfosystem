198edaa871c1c9bc91be03955681c27def2637ac
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2023-08-01 16:24:02 +0530

    Create App.vue

---
+++ services/FE/App.vue
@@ -0,0 +1,31 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <StudentsList />
+    <ReportForm />
+    <AdminLogin />
+  </div>
+</template>
+
+<script>
+import StudentsList from './components/StudentsList.vue';
+import ReportForm from './components/ReportForm.vue';
+import AdminLogin from './components/AdminLogin.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    StudentsList,
+    ReportForm,
+    AdminLogin
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 20px;
+}
+</style>
